6adff5f8b20de72531e929e6d3e90a1ad08ae460
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-04-10 20:06:24 -0300

    move code

--- src/components/NonnaAppBar.js
+++ src/components/NonnaAppBar.js
@@ -7,6 +7,30 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import FaceIcon from '@mui/icons-material/Face';
 
+export default function NonnaAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <FaceIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            La Nonna
+          </Typography>
+          <Button color="inherit" className={classes.menuButton}>Recetas</Button>
+          <Button color="inherit" className={classes.menuButton}>Categorías</Button>
+          <Button className={classes.menuButtonSecondary}>Ingresar</Button>
+          <Button className={classes.menuButtonSecondary}>Mi Perfil</Button>
+          <Button className={classes.menuButtonPrimary}>Registrate</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -41,27 +65,3 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "5px"
   }
 }));
-
-export default function NonnaAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <FaceIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            La Nonna
-          </Typography>
-          <Button color="inherit" className={classes.menuButton}>Recetas</Button>
-          <Button color="inherit" className={classes.menuButton}>Categorías</Button>
-          <Button className={classes.menuButtonSecondary}>Ingresar</Button>
-          <Button className={classes.menuButtonSecondary}>Mi Perfil</Button>
-          <Button className={classes.menuButtonPrimary}>Registrate</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
